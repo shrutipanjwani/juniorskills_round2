4ac0a31a1a13301fab0f1354d1b5d9f235e9161a
Shruti Panjwani <67150258+shrutipanjwani@users.noreply.github.com>
2021-08-12 11:24:38 +0530

    web technologies juniorskills round 2 test project - solution

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links .nav-item");
+
+hamburger.addEventListener("click", () => {
+	//Animate Links
+	navLinks.classList.toggle("open");
+	links.forEach((link) => {
+		link.classList.toggle("fade");
+	});
+
+	//Hamburger Animation
+	hamburger.classList.toggle("toggle");
+});
